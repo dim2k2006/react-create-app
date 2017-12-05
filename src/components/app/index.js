086cb1f1699f68dcb26007d29207c917548bea64
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-12-05 21:28:18 +0300

    Add red color identify algorithm

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Layout from '../layout';
+import randomInteger from '../../utils/random';
 import 'normalize.css';
 import './style.css';
 
@@ -19,6 +20,7 @@ class App extends Component {
         this.state = {
             isLoading: false,
             source: '',
+            palette: [],
             result: 0
         };
     }
@@ -51,7 +53,7 @@ class App extends Component {
                     source: dataURL,
                     result: 0
                 });
-            }, 4000);
+            }, 3000);
         });
 
         reader.readAsDataURL(input.files[0]);
@@ -62,11 +64,31 @@ class App extends Component {
      */
     generatePalette() {
         const image = this.image;
-        console.log('image:', image);
-
         const palette = colorThief.getPalette(image);
+        let result = 0;
+        const ratioLimit = 25;
 
-        console.log(palette);
+        for (const color of palette) {
+            const red = color[0];
+            const green = color[1];
+            const blue = color[2];
+
+            const greenRatio = Math.floor(green / red * 100);
+            const blueRatio = Math.floor(blue / red * 100);
+
+            if (greenRatio <= ratioLimit && blueRatio <= ratioLimit) {
+                result = randomInteger(81, 100);
+                break;
+            } else {
+                result = randomInteger(1, 79);
+            }
+        }
+
+        this.setState({
+            isLoading: false,
+            result,
+            palette
+        });
     }
 
     render() {
@@ -78,6 +100,21 @@ class App extends Component {
                     source={this.state.source}
                     result={this.state.result}
                 />
+
+                <div className="app__palette">
+                    {
+                        this.state.palette.map((color, index) => {
+                            const bgcolor = `rgba(${color.join(',')}, 1)`;
+
+                            return (
+                                <div key={index} className="app__paletteItem">
+                                    <span style={{backgroundColor: bgcolor}} />
+                                    <i>{bgcolor}</i>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
 
                 <div className="app__image">
                     <img src={this.state.source} alt='some img' ref={(image) => this.image = image} />
